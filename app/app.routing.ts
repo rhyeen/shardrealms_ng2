22b0f41be60a07fe2b6e64c1fc45eb2bb1eeb475
Ryan Saunders <contact.ryan.saunders@gmail.com>
2016-09-24 18:24:45 -0600

    Forgot the routing module

---
+++ app/app.routing.ts
@@ -0,0 +1,33 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RootContainerComponent }  from './components/root-container.component';
+import { PlayerListContainerComponent }  from './components/player-list-container.component';
+import { TableContainerComponent }  from './components/table-container.component';
+
+const appRoutes: Routes = [
+  { path: 'players', component: PlayerListContainerComponent },
+  { path: 'player', component: PlayerListContainerComponent },
+  { path: 'player/:id', component: PlayerListContainerComponent },
+
+  { path: 'tables', component: TableContainerComponent },
+  { path: 'table', component: TableContainerComponent },
+  { path: 'table/:id', component: TableContainerComponent },
+
+  // { path: 'table/:name', component: RootContainerComponent },
+  // {
+  //   path: 'heroes',
+  //   component: RootContainerComponent,
+  //   data: {
+  //     title: 'Heroes List'
+  //   }
+  // // },
+  { path: '', component: RootContainerComponent }
+  // { path: '**', component: RootContainerComponent }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
